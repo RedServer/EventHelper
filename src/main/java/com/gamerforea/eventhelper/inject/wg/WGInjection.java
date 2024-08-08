7ffa185c343f285569568232c8aaf00bd8bb4ac1
TheAndrey <me@theandrey.name>
2024-08-08 12:06:35 +0200

    Use WorldGuard's RegionQuery

--- src/main/java/com/gamerforea/eventhelper/inject/wg/WGInjection.java
+++ src/main/java/com/gamerforea/eventhelper/inject/wg/WGInjection.java
@@ -2,9 +2,9 @@ package com.gamerforea.eventhelper.inject.wg;
 
 import com.gamerforea.eventhelper.inject.PluginInjection;
 import com.gamerforea.eventhelper.util.InjectionUtils;
-import com.sk89q.worldedit.Vector;
+import com.sk89q.worldguard.bukkit.RegionQuery;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
@@ -27,29 +27,31 @@ public final class WGInjection
 
 	public static final class Inj implements PluginInjection
 	{
+		private final RegionQuery query; // TheAndrey: Add field
+
+		public Inj()
+		{
+			query = WorldGuardPlugin.inst().getRegionContainer().createQuery(); // TheAndrey: Use RegionQuery
+		}
+
 		@Override
 		public boolean isInPrivate(World world, int x, int y, int z)
 		{
-			for (ProtectedRegion region : WorldGuardPlugin.inst().getRegionManager(world).getApplicableRegions(new Vector(x, y, z)))
-			{
-				if (!region.getId().equals(ProtectedRegion.GLOBAL_REGION))
-					return true;
-			}
-			return false;
+			return query.getApplicableRegions(new Location(world, x, y, z)).size() > 0;
 		}
 
 		@Override
 		public boolean isPrivateMember(Player player, int x, int y, int z)
 		{
 			WorldGuardPlugin wg = WorldGuardPlugin.inst();
-			return wg.getRegionManager(player.getWorld()).getApplicableRegions(new Vector(x, y, z)).isMemberOfAll(wg.wrapPlayer(player, true));
+			return query.getApplicableRegions(new Location(player.getWorld(), x, y, z)).isMemberOfAll(wg.wrapPlayer(player, true));
 		}
 
 		@Override
 		public boolean isPrivateOwner(Player player, int x, int y, int z)
 		{
 			WorldGuardPlugin wg = WorldGuardPlugin.inst();
-			return wg.getRegionManager(player.getWorld()).getApplicableRegions(new Vector(x, y, z)).isOwnerOfAll(wg.wrapPlayer(player, true));
+			return query.getApplicableRegions(new Location(player.getWorld(), x, y, z)).isOwnerOfAll(wg.wrapPlayer(player, true));
 		}
 	}
 }
